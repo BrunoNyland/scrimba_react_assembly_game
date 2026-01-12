2af587e631b7b0102f4444d5480c79e39f90ceef
Bruno <bruno-nyland@hotmail.com>
2026-01-12 15:58:14 -0400

    feat: adiciona lógica para selecionar palavras aleatórias e reiniciar o jogo

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Chip from './Chip'
 import Letter from './Letter'
 import Keyboard from './Keyboard'
 import clsx from 'clsx'
-import { getFarewellText } from './utils'
+import { getFarewellText, getRandomWord } from './utils'
 
 export default function App() {
-  const [currentWord, setCurrentWord] = useState("react")
+  const [currentWord, setCurrentWord] = useState(() => getRandomWord())
+  console.log(currentWord)
   const [guessedLetters, setGuessedLetters] = useState([])
 
   const wrongGuessedArray = guessedLetters.filter(letter => !currentWord.includes(letter))
@@ -17,7 +18,7 @@ export default function App() {
   const isGameOver = isGameLost || isGameWon
   const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
   const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
-
+  const numGuessesLeft = languages.length -1 -wrongGuessCount
 
   function renderGameStatus() {
     if (!isGameOver && isLastGuessIncorrect) {
@@ -40,6 +41,11 @@ export default function App() {
         </>
       )
     }
+  }
+
+  function startNewGame() {
+    setCurrentWord(getRandomWord())
+    setGuessedLetters([])
   }
 
   function addGuessedLetter(letter) {
@@ -82,12 +88,19 @@ export default function App() {
       </section>
 
       <section className="sr-only" aria-live="polite" role="status">
+        <p>
+          {currentWord.includes(lastGuessedLetter) ? 
+              `Correct! The letter ${lastGuessedLetter} is in the word.` : 
+              `Sorry, the letter ${lastGuessedLetter} is not in the word.`
+          }
+          You have {numGuessesLeft} attempts left.
+        </p>
         <p>Current word: {currentWord.split("").map(letter => guessedLetters.includes(letter) ? letter + "." : "blank.").join(" ")}</p>
       </section>
 
       <Keyboard onClick={addGuessedLetter} guessedLetters={guessedLetters} currentWord={currentWord} isGameOver={isGameOver}/>
 
-      {isGameOver ? <button className="new-game">New Game</button> : null}
+      {isGameOver ? <button onClick={startNewGame} className="new-game">New Game</button> : null}
     </main>
   )
 }
